bc41fd5afbcae1b0c98205e9fc8a09705f93d8c3
donghyeokJO <49118254+donghyeokJO@users.noreply.github.com>
2022-07-07 02:37:31 +0900

    mobile sidebar

--- src/components/DashBoardHeader.js
+++ src/components/DashBoardHeader.js
@@ -13,20 +13,26 @@ class DashboardHeader extends React.Component {
         Name: "",
         museum_name: 0,
         IsAuth: false,
+        mopen: false,
+        mobilemenu_cls: `${style.gnb} ${style.mobileMenu}`,
+        mbtn: '',
+        bgshow: false,
     };
+
+
 
     componentDidMount() {
         this.setState({
             Name: localStorage.getItem('userName'),
             museum_name: localStorage.getItem('museumName'),
-            IsAuth: (localStorage.getItem('access') === null ? false : true)
+            IsAuth: (localStorage.getItem('access') === null ? false : true),
         });
     };
 
 
     render() {
         return (
-            <header id={style.header2}>
+            <header className={style.header2}>
                 <div id={style.header}>
                     <h1 className={style.logo}>스마트과학관</h1>
                     <div className={style.gnb}>
@@ -56,32 +62,52 @@ class DashboardHeader extends React.Component {
                             </li>
                         </ul>
                     </div>
-                    <a href="#" className={style.mopen} title="모바일 메뉴 열기"></a>
-                    <a href="#" className={style.mclose} title="모바일 메뉴 닫기"></a>
+                    {!this.state.mopen ?
+                        <a href="#" className={style.mopen} title="모바일 메뉴 열기" onClick={() => this.setState({ mopen: !this.state.mopen, bgshow: !this.state.bgshow, mobilemenu_cls: `${style.gnb} ${style.mobileMenu} ${style.on}` })}></a>
+                        : <a href="#" className={style.mclose} title="모바일 메뉴 닫기" onClick={() => this.setState({ mopen: !this.state.mopen, bgshow: !this.state.bgshow, mobilemenu_cls: `${style.gnb} ${style.mobileMenu}` })}></a>
+                    }
+                    {/* <a href="#" className={style.mopen} title="모바일 메뉴 열기"></a>
+                    <a href="#" className={style.mclose} title="모바일 메뉴 닫기"></a> */}
+
                 </div>
-                {/* {`${style.mainVisu} ${style.clearfix}`} */}
-                <div className={`${style.gnb} ${style.mobileMenu}`}>
+                <div className={this.state.mobilemenu_cls}>
                     <nav id={style.subNav}>
                         <div className={style.user}>
                             <ul className={style.clearfix}>
-                                <li><i className="fas fa-user"></i>김홍삼님</li>
-                                <li><a href="#" title="로그아웃하기"><i className="fas fa-unlock"></i>로그아웃</a></li>
+                                <li><i className="fas fa-user"></i>{this.state.Name}님</li>
+                                <li><a href="#" title="로그아웃하기" onClick={function () { localStorage.clear(); window.location.href = '/' }}><i className="fas fa-unlock"></i>로그아웃</a></li>
                             </ul>
                         </div>
                     </nav>
                     <ul className={style.menu}>
-                        <li className={style.active}><a href="#" title="대시보드 메인으로 이동"><i className="fas fa-home"></i><span>대시보드</span></a></li>
-                        <li className={style.manage}>
-                            <a href="ManageDrawing.html" title="전시관 관리 페이지로 이동"><i className="far fa-edit"></i><span>전시관 관리</span></a>
+                        <li className={`home ${this.props.main ? style.active : null}`}>
+                            <a href="/dashboard" title="대시보드 메인으로 이동">
+                                {/* <i className="fas fa-home"></i> */}
+                                {/* <FontAwesomeIcon icon={faHome} style={{color:'white'}}/> */}
+                                대시보드
+                            </a>
+                        </li>
+                        <li className={`${style.manage} ${this.props.exhibition ? style.active : null}`}>
+                            <a href="/exhibition" title="전시관 관리 페이지로 이동">
+                                {/* <i className="far fa-edit"></i> */}
+                                전시관 관리
+                            </a>
                             <ul className={style.submenu}>
-                                <li><a href="ManageDrawing.html" title="전시관 도면 관리 페이지로 이동">전시관 도면 관리</a></li>
-                                <li><a href="ManageInfo.html" title="전시관 정보 관리 페이지로 이동">전시관 정보 관리</a></li>
+                                <li className={this.props.ex1 ? style.active : null}><a href="/exhibition" title="전시관 도면 관리 페이지로 이동">전시관 도면 관리</a></li>
+                                <li className={this.props.ex2 ? style.active : null}><a href="/inner-exhibition" title="전시관 정보 관리 페이지로 이동">전시관 정보 관리</a></li>
                             </ul>
                         </li>
-                        <li className="event"><a href="Event.html" title="이벤트 페이지로 이동"><i className="fas fa-gift"></i><span>이벤트</span></a></li>
+                        <li className={`${style.event} ${this.props.event ? style.active : null}`}>
+                            <a href="/event" title="이벤트 페이지로 이동">
+                                {/* <i className="fas fa-gift"></i> */}
+                                이벤트
+                            </a>
+                        </li>
                     </ul>
                 </div>
-                <div className={style.bgshadow}></div>
+                <div className={this.state.bgshow ? style.bgshadowon : style.bgshadow} ></div>
+                {/* {`${style.mainVisu} ${style.clearfix}`} */}
+
             </header>
         );
 
