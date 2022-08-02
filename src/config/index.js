40ae949fae875fc252e3bc362b0627954bb5232c
donghyeokJO <joeastgur@korea.ac.kr>
2022-08-02 18:23:14 +0900

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -6,7 +6,7 @@
 const baseURL =
     window.location.origin === "http://localhost:3000"
         // ? "http://127.0.0.1:8000"
-        ? "http://59.19.102.174:8888"
-        : "http://59.19.102.174:8888";
+        ? "http://locslab.com"
+        : "http://locslab.com";
 
 export { baseURL };
